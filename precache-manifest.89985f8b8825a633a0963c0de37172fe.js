91b2c61cf0f16b403f4ed044ba8869afe4469c37
Kyle <kk.mackinnon@gmail.com>
2019-06-14 15:30:25 +1200

    First version
    Woot

---
+++ precache-manifest.89985f8b8825a633a0963c0de37172fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f01c63946637912efeeb1a8e67aaca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3f4786e38a1677b2004",
+    "url": "/static/css/main.c77fa13f.chunk.css"
+  },
+  {
+    "revision": "4ef2a6837c62b0a7b7cc",
+    "url": "/static/js/2.c56174e8.chunk.js"
+  },
+  {
+    "revision": "d3f4786e38a1677b2004",
+    "url": "/static/js/main.f1d60260.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
